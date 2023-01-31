5e52cda9326eeec759f42602d950670567c39ab1
kartik <kartikmakhloga20@gmail.com>
2023-01-31 17:59:41 +0530

    adding files

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ let server = express();
 //ye line server ko chalu krdeti hai
 //ek port pr
 server.listen(3000);
+
+
+// "/" is a path here
+server.get("/",function(req,res){
+      res.send("hi");
+});
+server.get("/movies",function(req,res){
+      res.send("movies data from server");
+});
+server.get("/genre",function(req,res){
+      res.send("genre data from server");
+});
+// http://localhost:3000/
+
