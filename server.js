6ccd19bac1947bd2de392a7691668293a0d012f1
kartik <kartikmakhloga20@gmail.com>
2023-01-31 17:16:44 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require("express");
+
+//ek naya server bnati hai but ye sirf create krti use chalu nahi krti
+let server = express();
+
+//ye line server ko chalu krdeti hai
+//ek port pr
+server.listen(3000);
